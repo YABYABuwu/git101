e7e85e85963c6c5b3625980c6909f4a2d88da23e
Teerawat Saenum <saenumteerawat@gail.com>
2026-01-04 22:45:42 +0700

    add insertion sort and selection sort

---
+++ selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(self, numbers):
+    pass  # write your code hear
+
+
+if __name__ == "__main__":
+    numbers = list(map(int, input("Enter integer number with space: ")))
+    sorted_numbers = selection_sort(numbers)
+    print("Sorted number is", sorted_numbers)
